4932fe5ea2ecf95a4bd0c55c9a743bd1138ce50d
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2022-11-26 09:55:10 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      brand: "#B2294B",
+      "brand-dark": "#851F38",
+      "brand-light": "#F7EAED",
+      secondary1: "#E4BC28",
+      "secondary1-light": "#FCF8E9",
+      secondary2: "#32C08D",
+      "secondary2-light": "#EAF9F4",
+      black: "#1E1E1E",
+      "grey-content": "#878787",
+      "grey-border": "#E0E0E0",
+      "grey-foreground": "#F0F0F0",
+      "grey-background": "#FAFAFA",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
 };
